293f3524318ab7399b069f1e009d22011eaa6786
ryutamaki <ryu.mochi@gmail.com>
2017-02-04 20:55:35 +0900

    Add app.vue

---
+++ template/src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+    {{#router}}
+    <router-view></router-view>
+    {{/router}}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue = require("vue");
+
+export default {
+  name: "app",
+} as Vue.ComponentOptions<Vue>;
+</script>
+
+<style lang="scss">
+@import "./scss/_all.scss";
+</style>
